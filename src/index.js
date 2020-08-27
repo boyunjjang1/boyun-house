4cd28584c994a9ae96ded28415f1c0621cfd89c2
흐르미 <boyumi98@gmail.com>
2020-08-27 11:21:20 +0900

    first commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Shop from "./pages/shop";
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Shop />,
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
---
+++ src/pages/shop/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Shop =() => {
+    return (
+        <div>오늘의딜</div>
+    )
+}
+
+export default Shop;
