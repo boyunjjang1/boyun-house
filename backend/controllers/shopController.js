efd711ef0272aab0f1f31c197c5e91dd1e434586
흐르미 <boyumi98@gmail.com>
2020-08-30 07:25:44 +0900

    집꾸미기 사이트 크롤링추가

--- backend/controllers/shopController.js
+++ backend/controllers/shopController.js
@@ -1,5 +1,46 @@
 const { pool } = require('../config/database');
+const axios = require("axios");
+const cheerio = require("cheerio");
+const log = console.log;
+
+ const getHtml = async () => {
+    try {
+        return await axios.get("https://m.ggumim.co.kr/furniture/more/c2");
+    } catch (error) {
+        console.error(error);
+    }
+};
+
 
 exports.productsList = async (req,res) => {
     
+}
+
+exports.productsGet = (req,res) => {
+
+    getHtml()
+    .then(html => {
+        
+        let ulList = [];
+        const $ = cheerio.load(html.data);
+        const $bodyList = $("div.furniture-item").children("div.item-desc");
+
+        $bodyList.each(function(i, elem) {
+        ulList[i] = {
+            item_brand: $(this).find('div.item-brand').text(),
+            item_name: $(this).find('div.item-name').text(),
+            item_price: $(this).find('div.item-price').text()
+        };
+        });
+
+
+
+        const data = ulList.filter(n => n.item_name);
+        return data;
+    })
+    .then(res => log(res));
+
+
+    return;
+
 }
--- backend/routes/shopRoute.js
+++ backend/routes/shopRoute.js
@@ -1,5 +1,6 @@
 module.exports = function(app){
     const shop = require('../controllers/shopController');
     app.get('/products/:page_no', shop.productsList);
-    
+    app.get('/pd', shop.productsGet);
+
 };
